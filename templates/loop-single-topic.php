2e2a9ea0fb72dd9da35de0c686e0669f31775358
ida-work <i21155388@gmail.com>
2025-08-11 07:13:56 +0900

    子フォーラムも一覧に表示されるように修正

--- templates/loop-single-topic.php
+++ templates/loop-single-topic.php
@@ -8,36 +8,36 @@
  */
 
 // Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 ?>
-
-<ul id="bbp-topic-<?php bbp_topic_id(); ?>" <?php bbp_topic_class(); ?>>
+<ul id="bbp-topic-<?php bbp_topic_id(); ?>" <?php bbp_topic_class(); ?>
+	style="background-color: transparent; border-top: 2px solid #e2e8f0;">
 	<li class="bbp-topic-title">
 
-		<?php if ( bbp_is_user_home() ) : ?>
+		<?php if (bbp_is_user_home()) : ?>
 
-			<?php if ( bbp_is_favorites() ) : ?>
+			<?php if (bbp_is_favorites()) : ?>
 
 				<span class="bbp-row-actions">
 
-					<?php do_action( 'bbp_theme_before_topic_favorites_action' ); ?>
+					<?php do_action('bbp_theme_before_topic_favorites_action'); ?>
 
-					<?php bbp_topic_favorite_link( array( 'before' => '', 'favorite' => '+', 'favorited' => '&times;' ) ); ?>
+					<?php bbp_topic_favorite_link(array('before' => '', 'favorite' => '+', 'favorited' => '&times;')); ?>
 
-					<?php do_action( 'bbp_theme_after_topic_favorites_action' ); ?>
+					<?php do_action('bbp_theme_after_topic_favorites_action'); ?>
 
 				</span>
 
-			<?php elseif ( bbp_is_subscriptions() ) : ?>
+			<?php elseif (bbp_is_subscriptions()) : ?>
 
 				<span class="bbp-row-actions">
 
-					<?php do_action( 'bbp_theme_before_topic_subscription_action' ); ?>
+					<?php do_action('bbp_theme_before_topic_subscription_action'); ?>
 
-					<?php bbp_topic_subscription_link( array( 'before' => '', 'subscribe' => '+', 'unsubscribe' => '&times;' ) ); ?>
+					<?php bbp_topic_subscription_link(array('before' => '', 'subscribe' => '+', 'unsubscribe' => '&times;')); ?>
 
-					<?php do_action( 'bbp_theme_after_topic_subscription_action' ); ?>
+					<?php do_action('bbp_theme_after_topic_subscription_action'); ?>
 
 				</span>
 
@@ -45,72 +45,72 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php endif; ?>
 
-		<?php do_action( 'bbp_theme_before_topic_title' ); ?>
+		<?php do_action('bbp_theme_before_topic_title'); ?>
 
 		<a class="bbp-topic-permalink text-lg font-bold !text-gray-800 hover:!text-gray-600 hover:!underline inline-block mb-4" href="<?php bbp_topic_permalink(); ?>"><?php bbp_topic_title(); ?></a>
 
-		<?php do_action( 'bbp_theme_after_topic_title' ); ?>
+		<?php do_action('bbp_theme_after_topic_title'); ?>
 
 		<?php bbp_topic_pagination(); ?>
 
-		<?php do_action( 'bbp_theme_before_topic_meta' ); ?>
+		<?php do_action('bbp_theme_before_topic_meta'); ?>
 
 		<p class="bbp-topic-meta">
 
-			<?php do_action( 'bbp_theme_before_topic_started_by' ); ?>
+			<?php do_action('bbp_theme_before_topic_started_by'); ?>
 
-			<span class="bbp-topic-started-by"><?php printf( esc_html__( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?></span>
+			<span class="bbp-topic-started-by"><?php printf(esc_html__('Started by: %1$s', 'bbpress'), bbp_get_topic_author_link(array('size' => '14'))); ?></span>
 
-			<?php do_action( 'bbp_theme_after_topic_started_by' ); ?>
+			<?php do_action('bbp_theme_after_topic_started_by'); ?>
 
-			<?php if ( ! bbp_is_single_forum() || ( bbp_get_topic_forum_id() !== bbp_get_forum_id() ) ) : ?>
+			<?php if (! bbp_is_single_forum() || (bbp_get_topic_forum_id() !== bbp_get_forum_id())) : ?>
 
-				<?php do_action( 'bbp_theme_before_topic_started_in' ); ?>
+				<?php do_action('bbp_theme_before_topic_started_in'); ?>
 
-				<span class="bbp-topic-started-in"><?php printf( esc_html__( 'in: %1$s', 'bbpress' ), '<a href="' . bbp_get_forum_permalink( bbp_get_topic_forum_id() ) . '">' . bbp_get_forum_title( bbp_get_topic_forum_id() ) . '</a>' ); ?></span>
-				<?php do_action( 'bbp_theme_after_topic_started_in' ); ?>
+				<span class="bbp-topic-started-in"><?php printf(esc_html__('in: %1$s', 'bbpress'), '<a href="' . bbp_get_forum_permalink(bbp_get_topic_forum_id()) . '">' . bbp_get_forum_title(bbp_get_topic_forum_id()) . '</a>'); ?></span>
+				<?php do_action('bbp_theme_after_topic_started_in'); ?>
 
 			<?php endif; ?>
 
-			<?php 
+			<?php
 			// トピックのタグを表示
-			$topic_tags = get_the_terms( bbp_get_topic_id(), 'topic-tag' );
-			if ( ! empty( $topic_tags ) && ! is_wp_error( $topic_tags ) ) : ?>
+			$topic_tags = get_the_terms(bbp_get_topic_id(), 'topic-tag');
+			if (! empty($topic_tags) && ! is_wp_error($topic_tags)) : ?>
 				<span class="bbp-topic-tags ml-4">
-					<?php _e( 'タグ : ', 'bbpress' ); ?>
-					<?php foreach ( $topic_tags as $index => $tag ) : ?>
-						<a href="<?php echo get_term_link( $tag ); ?>" class="bbp-topic-tag bg-blue-100 text-blue-800 px-2 py-1 rounded text-xs mr-1 hover:bg-blue-200 transition-colors"><?php echo esc_html( $tag->name ); ?></a><?php echo ( $index < count( $topic_tags ) - 1 ) ? ', ' : ''; ?>
+					<?php _e('タグ : ', 'bbpress'); ?>
+					<?php foreach ($topic_tags as $index => $tag) : ?>
+						<a href="<?php echo get_term_link($tag); ?>" class="bbp-topic-tag bg-blue-100 text-blue-800 px-2 py-1 rounded text-xs mr-1 hover:bg-blue-200 transition-colors"><?php echo esc_html($tag->name); ?></a><?php echo ($index < count($topic_tags) - 1) ? ', ' : ''; ?>
 					<?php endforeach; ?>
 				</span>
 			<?php endif; ?>
 
 		</p>
 
-		<?php do_action( 'bbp_theme_after_topic_meta' ); ?>
+		<?php do_action('bbp_theme_after_topic_meta'); ?>
 
 		<?php bbp_topic_row_actions(); ?>
 
 	</li>
 
-	<li class="bbp-topic-voice-count"><?php bbp_topic_voice_count(); ?></li>
+	<li class="bbp-topic-voice-count !text-gray-500">参加者: <?php bbp_topic_voice_count(); ?></li>
 
-	<li class="bbp-topic-reply-count"><?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></li>
+	<li class="bbp-topic-reply-count !text-gray-500">返信: <?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></li>
 
 	<li class="bbp-topic-freshness">
 
-		<?php do_action( 'bbp_theme_before_topic_freshness_link' ); ?>
+		<?php do_action('bbp_theme_before_topic_freshness_link'); ?>
 
 		<?php bbp_topic_freshness_link(); ?>
 
-		<?php do_action( 'bbp_theme_after_topic_freshness_link' ); ?>
+		<?php do_action('bbp_theme_after_topic_freshness_link'); ?>
 
 		<p class="bbp-topic-meta">
 
-			<?php do_action( 'bbp_theme_before_topic_freshness_author' ); ?>
+			<?php do_action('bbp_theme_before_topic_freshness_author'); ?>
 
-			<span class="bbp-topic-freshness-author"><?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'size' => 14 ) ); ?></span>
+			<span class="bbp-topic-freshness-author"><?php bbp_author_link(array('post_id' => bbp_get_topic_last_active_id(), 'size' => 14)); ?></span>
 
-			<?php do_action( 'bbp_theme_after_topic_freshness_author' ); ?>
+			<?php do_action('bbp_theme_after_topic_freshness_author'); ?>
 
 		</p>
 	</li>
